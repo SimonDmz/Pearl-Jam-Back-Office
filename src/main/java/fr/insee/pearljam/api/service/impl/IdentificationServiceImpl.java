64ec9036925a71eff2edb31d80f2493274f4e18c
Simon Demazière <simon.demaziere@insee.fr>
2022-06-13 08:26:49 +0200

    missing annotation on service implementation

--- src/main/java/fr/insee/pearljam/api/service/impl/IdentificationServiceImpl.java
+++ src/main/java/fr/insee/pearljam/api/service/impl/IdentificationServiceImpl.java
@@ -1,11 +1,13 @@
 package fr.insee.pearljam.api.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import fr.insee.pearljam.api.domain.Identification;
 import fr.insee.pearljam.api.repository.IdentificationRepository;
 import fr.insee.pearljam.api.service.IdentificationService;
 
+@Service
 public class IdentificationServiceImpl implements IdentificationService {
 
     @Autowired
